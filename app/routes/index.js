5bcf3a680497b3b6714b7a3dbbc5c731485a102b
Brendan Grubb and Philip Putnam <philip@>
2017-03-20 16:36:55 -0700

    add announcement component with dynamic announcement pages

--- app/routes/index.js
+++ app/routes/index.js
@@ -2,8 +2,12 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model() {
-    return this.store.findAll('rental');
+    return {
+    rental: this.store.findAll('rental'),
+    announcement: this.store.findAll('announcement')
+    }
   },
+
   actions: {
     saveRental(params) {
       var newRental = this.store.createRecord('rental', params);
